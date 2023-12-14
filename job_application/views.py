ba67fb0d07008fe33b9d97dcaa9a76bf5c7c873f
Regidan <regidancodes@gmail.com>
2023-12-14 12:30:35 +0530

    Created base template and connect to index and about.html

--- job_application/views.py
+++ job_application/views.py
@@ -21,3 +21,7 @@ def index(request):
             email_message.send()
             messages.success(request, 'Form submitted successfully')
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
