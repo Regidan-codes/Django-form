f7dfa4f68125a9ced134b42846dc7eeca833a280
Regidan <regidancodes@gmail.com>
2023-12-14 12:11:45 +0530

    Created admin interface

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'huberwrat@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
